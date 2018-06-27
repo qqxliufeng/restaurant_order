f588e7593a3147751fa785c8877566a454130d25
lf <lf@appledeiMac.lan>
2018-06-27 17:43:31 +0800

    first commit on 2018-06-27

--- app/src/main/java/com/android/ql/restaurant/MainActivity.kt
+++ app/src/main/java/com/android/ql/restaurant/MainActivity.kt
@@ -1,8 +1,7 @@
-package com.android.ql.lf
+package com.android.ql.restaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
