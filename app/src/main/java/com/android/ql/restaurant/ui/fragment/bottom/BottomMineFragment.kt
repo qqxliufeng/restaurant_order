ab33bddbac5ab050302b579feb8652bf7f7bac42
lf <lf@appledeiMac.local>
2018-07-16 18:39:23 +0800

    修改点页面

--- app/src/main/java/com/android/ql/restaurant/ui/fragment/bottom/BottomMineFragment.kt
+++ app/src/main/java/com/android/ql/restaurant/ui/fragment/bottom/BottomMineFragment.kt
@@ -1,7 +1,10 @@
 package com.android.ql.restaurant.ui.fragment.bottom
 
+import android.app.Dialog
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.android.ql.restaurant.R
 import com.android.ql.restaurant.ui.activity.FragmentContainerActivity
 import com.android.ql.restaurant.ui.fragment.base.BaseFragment
@@ -9,9 +12,14 @@ import com.android.ql.restaurant.ui.fragment.mine.LoginFragment
 import com.android.ql.restaurant.ui.fragment.mine.MineContactFragment
 import com.android.ql.restaurant.ui.fragment.mine.MineOrderListFragment
 import com.android.ql.restaurant.ui.fragment.mine.MinePersonalInfoFragment
+import com.android.ql.restaurant.utils.setDiffColorText
 import kotlinx.android.synthetic.main.fragment_mine_layout.*
 
 class BottomMineFragment : BaseFragment() {
+
+    private val notifyDialog by lazy {
+        Dialog(mContext)
+    }
 
     override fun getLayoutId() = R.layout.fragment_mine_layout
 
@@ -33,5 +41,14 @@ class BottomMineFragment : BaseFragment() {
         mTvMineCallRes.setOnClickListener {
             FragmentContainerActivity.from(mContext).setNeedNetWorking(true).setTitle("聯繫方式").setClazz(MineContactFragment::class.java).start()
         }
+
+        val contentView = View.inflate(mContext,R.layout.dialog_notify_layout,null)
+        val tv_num = contentView.findViewById<TextView>(R.id.mTvNotifyDialogNum)
+        tv_num.setDiffColorText("您的號碼","A10",color2 = "#880015")
+        contentView.findViewById<Button>(R.id.mBtNotifyDialogSubmit).setOnClickListener {
+            notifyDialog.dismiss()
+        }
+        notifyDialog.setContentView(contentView)
+        notifyDialog.show()
     }
 }
